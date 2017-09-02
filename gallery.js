997779c8a20f1fedb18ef76a4c942b2cb32390b9
Oleksandr <scriptchevy@gmail.com>
2017-09-02 16:55:04 +0300

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    var slideIndex = 1;
+    showSlides(slideIndex);
+    document.getElementsByClassName("prev-slide")[0].addEventListener("click", function (event) {
+        event.preventDefault();
+        showSlides(slideIndex += -1);
+    });
+    document.getElementsByClassName("next-slide")[0].addEventListener("click", function (event) {
+        event.preventDefault();
+        showSlides(slideIndex += 1);
+    });
+
+    function showSlides(n) {
+        var slides = document.getElementsByClassName("slides");
+        if (n > slides.length) { slideIndex = 1 }
+        if (n < 1) { slideIndex = slides.length }
+        for (var i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slides[slideIndex - 1].style.display = "block";
+    }
+})();
---
+++ script.js
@@ -0,0 +1,19 @@
+﻿document.getElementById("showMap").addEventListener("click", function () {
+    modalMap = document.getElementsByClassName("modal-map")[0].style.display = "block";
+});
+
+document.getElementById("closeMap").addEventListener("click", function () {
+    modalMap = document.getElementsByClassName("modal-map")[0].style.display = "none";
+});
+
+document.getElementById("showForm").addEventListener("click", function () {
+    document.getElementsByClassName("modal-form-box")[0].style.display = "block";
+});
+
+document.getElementById("closeForm").addEventListener("click", function () {
+    document.getElementsByClassName("modal-form-box")[0].style.display = "none";
+});
+
+document.getElementsByClassName("top-menu-icon")[0].addEventListener("click", function () {
+    document.getElementById("show-top-nav").classList.toggle("top-nav-list");
+});
