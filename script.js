4e6e76e848592ef9d5b55895f40b8438e9c19f30
Oleksandr <scriptchevy@gmail.com>
2017-09-02 17:04:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-﻿document.getElementById("showMap").addEventListener("click", function () {
-    modalMap = document.getElementsByClassName("modal-map")[0].style.display = "block";
-});
+window.addEventListener("load", function () {
+    var modalMap = document.getElementsByClassName("modal-map")[0];
+    var signIn = document.getElementsByClassName("modal-form-box")[0];
+    
+    document.getElementById("showMap").addEventListener("click", function () {
+        modalMap.style.display = "block";
+    });
 
-document.getElementById("closeMap").addEventListener("click", function () {
-    modalMap = document.getElementsByClassName("modal-map")[0].style.display = "none";
-});
+    document.getElementById("closeMap").addEventListener("click", function () {
+        modalMap.style.display = "none";
+    });
 
-document.getElementById("showForm").addEventListener("click", function () {
-    document.getElementsByClassName("modal-form-box")[0].style.display = "block";
-});
+    document.getElementById("showForm").addEventListener("click", function () {
+        signIn.style.display = "block";
+    });
 
-document.getElementById("closeForm").addEventListener("click", function () {
-    document.getElementsByClassName("modal-form-box")[0].style.display = "none";
-});
+    document.getElementById("closeForm").addEventListener("click", function () {
+        signIn.style.display = "none";
+    });
 
-document.getElementsByClassName("top-menu-icon")[0].addEventListener("click", function () {
-    document.getElementById("show-top-nav").classList.toggle("top-nav-list");
+    document.getElementsByClassName("top-menu-icon")[0].addEventListener("click", function () {
+        document.getElementById("show-top-nav").classList.toggle("top-nav-list");
+    });
 });
